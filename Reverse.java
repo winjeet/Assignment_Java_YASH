b59a75d3af59f85fae8e6e382020caab6a718544
winjeet <60110233+winjeet@users.noreply.github.com>
2021-08-31 15:56:40 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,11 +1,10 @@
 class Reverse{
 	public static void main(String[] args){
-	int num=153, n=0;
-	n = num;
-	while(n>0){
-	p = n%10;
+	int num=153;
+	while(num>0){
+	p = num%10;
 	t = (t*10)+p;
-	n = n/10;
+	num = num/10;
 	}
 	System.out.println("reverse of "+num +" = "+t);
 }
